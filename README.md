afcbb1a359f53f9116dbf94bb83415b7529bc0bc
asutosh29 <asutosh29aaz@gmail.com>
2025-08-21 21:13:53 +0530

    Add initial Magnikaru backend

---
+++ README.md
@@ -0,0 +1,30 @@
+# Magnikaru
+Simple Chess Server using flask, chessboard2.js and chess.js
+
+# Setup Instructions
+You 2 choices
+
+## 1. Docker
+```
+> docker compose up --build
+```
+
+## 2. Manual Installation
+## Environment
+```
+> virtualenv venv
+> .\venv\Scripts\activate
+```
+
+## Install Dependancies
+```
+> .\venv\Scripts\activate
+```
+
+## Run the Server
+```
+> flask run
+```
+
+<hr/>
+Made with ❣️by AMX
---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+import chess
+import random
+
+app = Flask(__name__)
+CORS(app)
+
+# CHESS FUNCTIONS
+def make_random_move(current_fen):
+    board = chess.Board(current_fen)
+    print(board)
+    leg_moves = list(board.legal_moves)
+    print(f"Moves: {leg_moves}")
+    if len(leg_moves)==0:
+        return ""
+    move_idx = random.randint(0,len(leg_moves)-1)
+    # move_idx = 0
+    uci_move = leg_moves[move_idx]
+    
+    san_move = board.san(uci_move)
+    return san_move
+
+
+# ROUTES
+@app.route('/') 
+@app.route('/botplay') 
+def botplay():
+    return render_template("PlayerBot.html")
+
+@app.route('/api/random_move', methods=['POST']) 
+def random_move():
+    '''
+    {
+        "pgn":"",
+        "fen":""
+    }
+    '''
+    data = request.json
+    move = make_random_move(data['fen'])
+    print(f"Random Move from server: {move}")
+    return jsonify({
+        "move":move,
+        "message":f"Random move from server for input fen: {data['fen']}"
+    })
+
+
+if(__name__=="__main__"): 
+    PORT = 5000
+    print(f"Running server on port {PORT}")
+    app.run(host='0.0.0.0', port=PORT)
